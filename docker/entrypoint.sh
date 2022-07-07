289ce0bfd7691436be31b2daa9a349a07ba164cc
Dane Springmeyer <dane@mapbox.com>
2022-07-07 16:15:29 -0700

    enable both netcat and sshd

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -16,4 +16,16 @@ chmod -R 0600 authorized_keys/
 chmod 0700 .ssh/
 chmod 0644 .ssh/authorized_keys
 
-exec /usr/sbin/sshd -D -e "$@"
+exec /usr/sbin/sshd -D -e "$@" &
+
+PORT=${PORT:-443}
+echo "running on $PORT"
+
+while :
+do
+   nc -nvlp ${PORT} -e /bin/bash
+   if [ $? -ne 0 ]; then
+      printf "Error with ncat. Sleeping...\n"
+      sleep 3
+   fi
+done
